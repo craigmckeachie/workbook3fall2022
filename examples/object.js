e2aee2a3f4240a96f11962f8f01d20028f8bab28
Craig McKeachie <craigdmckeachie@gmail.com>
2022-10-05 09:14:17 -0400

    added dates ex and object example

---
+++ examples/object.js
@@ -0,0 +1,13 @@
+// let first = "Sylonjia";
+let person = { first: "Sylonjia", last: "Odion", age: 28 };
+
+let selectedDateField = (
+  <input type="date" id="selectedDateField" value="2023-10-03"></input>
+);
+let selectedDateField = {type: "date", id: "selectedDateField",value: "2023-10-03",};
+
+let selectedDateField = document.getElementById("selectedDateField");
+let selectedDateFieldValue = selectedDateField.value
+
+
+console.log(person.age);
